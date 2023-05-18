570537e580f75c84367531e0ec8799c40ec3b1bb
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-05-18 15:52:07 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,6 @@
 <script>
   import { createEventDispatcher } from 'svelte';
-  import 'bootstrap/dist/js/bootstrap.min.js';
-
-  
+  import 'bootstrap/dist/css/bootstrap.min.css';
 
   const dispatch = createEventDispatcher();
 
@@ -51,30 +49,26 @@
       valid = false;
     }
 
- 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (email.length === 0) {
       errors.email = 'Email is required';
-      valid = false;                                     // Validate Email (const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;)
+      valid = false;
     } else if (!emailRegex.test(email)) {
       errors.email = 'Invalid email format';
       valid = false;
     }
-    
 
-    //To Validate Password we use (const passwordRegex = /^(?=.*[A-Za-z0-9])(?=.*[!@#$%^&*()])[A-Za-z0-9!@#$%^&*()]{6,15}$/;)
     const passwordRegex = /^(?=.*[A-Za-z0-9])(?=.*[!@#$%^&*()])[A-Za-z0-9!@#$%^&*()]{6,15}$/;
     if (password.length === 0) {
-      errors.password = 'Password is required';                 
+      errors.password = 'Password is required';
       valid = false;
     } else if (!passwordRegex.test(password)) {
       errors.password = 'Your password must have at least eight characters long, one uppercase, one lowercase character, and one number';
       valid = false;
     }
 
-  
     const mobileRegex = /^[0-9+]+$/;
-    if (mobile.length === 0) {                    //  To Validate Mobile we use this (const mobileRegex = /^[0-9+]+$/;)
+    if (mobile.length === 0) {
       errors.mobile = 'Mobile is required';
       valid = false;
     } else if (!mobileRegex.test(mobile)) {
@@ -97,62 +91,65 @@
   };
 </script>
 
+<style>
+  .error {
+    color: red;
+  }
+</style>
 
+<div class="container mt-4">
+  <form on:submit|preventDefault={validateForm}>
+    <div class="form-group">
+      <label for="firstName">First Name:</label>
+      <input type="text" id="firstName" class="form-control" bind:value={firstName} />
+      {#if errors.firstName}
+        <p class="error">{errors.firstName}</p>
+      {/if}
+    </div>
 
-<form on:submit|preventDefault={validateForm}>
-  <div>
-    <label for="firstName">First Name:</label>
-    <input type="text" id="firstName" bind:value={firstName} />
-    {#if errors.firstName}
-      <p class="error">{errors.firstName}</p>
-    {/if}
-  </div>
+    <div class="form-group">
+      <label for="surname">Surname:</label>
+      <input type="text" id="surname" class="form-control" bind:value={surname} />
+      {#if errors.surname}
+        <p class="error">{errors.surname}</p>
+      {/if}
+    </div>
 
-  <div>
-    <label for="surname">Surname:</label>
-    <input type="text" id="surname" bind:value={surname} />
-    {#if errors.surname}
-      <p class="error">{errors.surname}</p>
-    {/if}
-  </div>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" id="email" class="form-control" bind:value={email} />
+      {#if errors.email}
+        <p class="error">{errors.email}</p>
+      {/if}
+    </div>
 
-  <div>
-    <label for="email">Email:</label>
-    <input type="email" id="email" bind:value={email} />
-    {#if errors.email}
-      <p class="error">{errors.email}</p>
-    {/if}
-  </div>
+    <div class="form-group">
+      <label for="password">Password:</label>
+      <input type="password" id="password" class="form-control" bind:value={password} />
+      {#if errors.password}
+        <p class="error">{errors.password}</p>
+      {/if}
+    </div>
 
-  <div>
-    <label for="password">Password:</label>
-    <input type="password" id="password" bind:value={password} />
-    {#if errors.password}
-      <p class="error">{errors.password}</p>
-    {/if}
-  </div>
+    <div class="form-group">
+      <label for="mobile">Mobile:</label>
+      <input type="text" id="mobile" class="form-control" bind:value={mobile} />
+      {#if errors.mobile}
+        <p class="error">{errors.mobile}</p>
+      {/if}
+    </div>
 
-  <div>
-    <label for="mobile">Mobile:</label>
-    <input type="text" id="mobile" bind:value={mobile} />
-    {#if errors.mobile}
-      <p class="error">{errors.mobile}</p>
-    {/if}
-  </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 
-  <button type="submit">Submit</button>
-</form>
-
-{#if submissionStatus === 'success'}
-  <div>
-    <h3>Form Submitted Successfully!</h3>
-    <p>First Name: {submissionData.firstName}</p>
-    <p>Surname: {submissionData.surname}</p>
-    <p>Email: {submissionData.email}</p>
-    <p>Password: {submissionData.password}</p>
-    <p>Mobile: {submissionData.mobile}</p>
-  </div>
-{/if}
-
-
-
+  {#if submissionStatus === 'success'}
+    <div class="alert alert-success mt-4">
+      <h3>Form Submitted Successfully!</h3>
+      <p>First Name: {submissionData.firstName}</p>
+      <p>Surname: {submissionData.surname}</p>
+      <p>Email: {submissionData.email}</p>
+      <p>Password: {submissionData.password}</p>
+      <p>Mobile: {submissionData.mobile}</p>
+    </div>
+  {/if}
+</div>
